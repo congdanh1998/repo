e9d4fe55845c57fb5ef208cc581688809ecbb288
anh2003maa <anh1998maa@gmail.com>
2017-05-22 22:22:52 +0700

    Update thuvien.h
    t sửa lại cái struct user với thêm 1 cái struct nữa

--- thuvien.h
+++ thuvien.h
@@ -1,22 +1,30 @@
+#pragma once
 #if !defined(THU_VIEN_H)
 #include <string>
 #define THU_VIEN_H
+#define MAX_ACCOUNTS 5
 using namespace std;
-typedef struct{
-	short ngay,thang,nam;
-}date;
-typedef struct user{
+typedef struct {
+	short ngay, thang, nam;
+} date;
+typedef struct {
 	string Tendangnhap;
 	string matkhau;
+} S_dangnhap;
+typedef struct {
+	int sothutu;
 	string hovaten;
-	string email;
 	date ngaysinh;
 	string sodienthoai;
+	string email;
 	bool giotinh;
+	string vaitro;
+	int sotaikhoan;
+	S_dangnhap taikhoan_matkhau[MAX_ACCOUNTS];
 }user;
 void dangnhap();
 void in();
 void indangki();
 void dangki();
-void xulichuoi(string &s,char a,int &x,int y,int maxlen = 29);
+void xulichuoi(string &s, char a, int &x, int y, int maxlen = 29);
 #endif
