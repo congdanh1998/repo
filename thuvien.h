54237e46ab1cf2bbcbd35de2e1a6f0eef29d6db0
anh2003maa <anh1998maa@gmail.com>
2017-06-09 13:37:46 +0700

    Update thuvien.h

--- thuvien.h
+++ thuvien.h
@@ -4,12 +4,13 @@
 #define THU_VIEN_H
 #define RONG 204
 #define MAX_cot 20
+#define GIA_1_CUON 15000
 using namespace std;
 typedef struct {
 	short ngay, thang, nam;
 } date;
 typedef struct {
-	int id,idu;
+	int id, idu;
 	string tendangnhap;
 	string matkhau;
 	int vaitro[3];
@@ -21,7 +22,7 @@ typedef struct {
 	date ngaysinh;
 	string mssv;
 	string email;
-	bool gioitinh,phat;
+	bool gioitinh, phat;
 } S_user;
 typedef struct {
 	int id;
@@ -31,7 +32,7 @@ typedef struct {
 	int conlai;
 } S_book;
 typedef struct {
-	int userid,bookid;
+	int userid, bookid;
 	date day;
 } S_Book_Order;
 void dangnhap();
@@ -42,16 +43,17 @@ void manhinhthongbao(string s);
 void manhinhlamviec();
 void sapxepgiam(int a[], int n);
 void manhinhdocgia(S_account& TAIKHOAN);
-void luachonsach(S_account A[],int n,int CSO);
+void luachonsach(S_account A[], int n, int CSO);
 void xulichuoi(string &s, char a, int &x, int y, int maxlen = 29);
-void trasach(S_account A[],int CSO,int N);
+void trasach(S_account A[], int CSO, int N);
 void doimatkhau(S_account A[], int CSO, int N);
-void bangtim(S_book A[],int a[],int n,S_account& S,int & flag,bool cp=true,bool MR=true);
+void bangtim(S_book A[], int a[], int n, S_account& S, int & flag, bool cp = true, bool MR = true);
 void timsach(S_account A[], int CSO, int N);
 void inDSmuon(S_account A[], int N);
 void them_sach();
-void xoasach(S_account A[],int N,int CSO);
-void chinhsuasach(S_account A[],int N,int CSO);
+void xoasach(S_account A[], int N, int CSO);
+void chinhsuasach(S_account A[], int N, int CSO);
 void DStre(S_account A[], int N);
 void thong_bao(S_account A[], int N, int CSO);
+void conv_longlong_money(string& str, int number);
 #endif
