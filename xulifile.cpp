c95a8a8d60b217309c435c5bc5168599f4be21c8
anh2003maa <anh1998maa@gmail.com>
2017-06-11 22:09:07 +0700

    Update xulifile.cpp

--- xulifile.cpp
+++ xulifile.cpp
@@ -47,26 +47,26 @@ void OverWriteAccount(S_account A[], int N) {
 	f.open("accounts_infor.txt");
 	for (int i = 0;i<N;i++) {//???????????????
 		f << setw(5) << left << A[i].id
-			<< setw(5) <<left << A[i].idu
+			<< setw(5) << left << A[i].idu
 			<< setw(25) << left << A[i].tendangnhap
-			<< setw(20) << left << A[i].matkhau 
+			<< setw(20) << left << A[i].matkhau
 			<< setw(5) << left << A[i].vaitro[0] <<
 			setw(5) << left << A[i].vaitro[1] <<
 			setw(5) << left << A[i].vaitro[2] <<
 			A[i].DS_muon[0] << " " << A[i].DS_muon[1] << " "
 			<< A[i].DS_muon[2] << " " << A[i].DS_muon[3] << " "
-			<< A[i].DS_muon[4] << " " << 
-			setw(5)<<A[i].khoa<<endl;
+			<< A[i].DS_muon[4] << " " <<
+			setw(5) << A[i].khoa << endl;
 	};
 	f.close();
 };
 int songayhientai(int year, int month, int day) {
-        if (month < 3) {
-            year--;
-            month += 12;
-        }
-        return 365*year + year/4 - year/100 + year/400 + (153*month - 457)/5 + day - 306;
-   }
+	if (month < 3) {
+		year--;
+		month += 12;
+	}
+	return 365 * year + year / 4 - year / 100 + year / 400 + (153 * month - 457) / 5 + day - 306;
+}
 void GetFileBookData(S_book A[], int N) {
 	ifstream f;
 	//A=new S_account[N];
@@ -83,57 +83,57 @@ void GetFileBookData(S_book A[], int N) {
 	}
 	f.close();
 };
-void OverWriteBook(S_book A[], int N){
+void OverWriteBook(S_book A[], int N) {
 	ofstream f;
 	f.open("books_infor.txt");
 	for (int i = 0;i<N;i++) {//???????????????
-		f << setw(5) << left << A[i].id<<
-			 setw(25) << left << A[i].tensach<<
-			 setw(20) << left << A[i].tacgia <<
-			 setw(20)<<left<<A[i].theloai<<
-			 setw(5)<<right<<A[i].conlai<< endl;
+		f << setw(5) << left << A[i].id <<
+			setw(25) << left << A[i].tensach <<
+			setw(20) << left << A[i].tacgia <<
+			setw(20) << left << A[i].theloai <<
+			setw(5) << right << A[i].conlai << endl;
 	};
 	f.close();
 };
-void GetfileOrderData(S_Book_Order A[],int &n, bool cp){
+void GetfileOrderData(S_Book_Order A[], int &n, bool cp) {
 	ifstream f;
 	f.open("abc.txt");
 	time_t baygio = time(0);
 	struct tm abc;
-	localtime_s(&abc,&baygio);
-	int ngay=abc.tm_mday,thang=abc.tm_mon+1,nam=abc.tm_year+1900;
-	int N=n,j=0;
-	for (int i=0;i<N;i++){
-		f>>A[j].userid>>A[j].bookid>>A[j].day.ngay>>A[j].day.thang>>A[j].day.nam;
-		if (cp){j++;}
-		else if(!cp){
-			int chenhlech=songayhientai(nam,thang,ngay)-songayhientai(A[j].day.nam,A[j].day.thang,A[j].day.ngay);
-			if (chenhlech>5){
+	localtime_s(&abc, &baygio);
+	int ngay = abc.tm_mday, thang = abc.tm_mon + 1, nam = abc.tm_year + 1900;
+	int N = n, j = 0;
+	for (int i = 0;i<N;i++) {
+		f >> A[j].userid >> A[j].bookid >> A[j].day.ngay >> A[j].day.thang >> A[j].day.nam;
+		if (cp) { j++; }
+		else if (!cp) {
+			int chenhlech = songayhientai(nam, thang, ngay) - songayhientai(A[j].day.nam, A[j].day.thang, A[j].day.ngay);
+			if (chenhlech>5) {
 				j++;
 			}
 		}
 	}
 	f.close();
-	n=j;
+	n = j;
 };
-void OverWriteOrder(S_Book_Order A[], int N){
+void OverWriteOrder(S_Book_Order A[], int N) {
 	ofstream f;
 	f.open("abc.txt");
 	for (int i = 0;i<N;i++) {
-		f<<setw(5)<<A[i].userid
-			<<setw(5)<<A[i].bookid
-			<<setw(5)<<A[i].day.ngay
-			<<setw(5)<<A[i].day.thang
-			<<setw(5)<<A[i].day.nam<<endl;
+		f << setw(5) << A[i].userid
+			<< setw(5) << A[i].bookid
+			<< setw(5) << A[i].day.ngay
+			<< setw(5) << A[i].day.thang
+			<< setw(5) << A[i].day.nam << endl;
 	};
 	f.close();
 };
-void GetFileUserData(S_user A[],int N){
+void GetFileUserData(S_user A[], int N) {
 	fstream f;
 	f.open("users_infor.txt", ios::in);
 	string data, NTNS;
-	
-	for(int i=0;i<N;i++){
+
+	for (int i = 0;i<N;i++) {
 		getline(f, data);
 		stringstream scin(data);
 		scin >> A[i].id;
@@ -143,28 +143,29 @@ void GetFileUserData(S_user A[],int N){
 		scin >> A[i].email;
 		scin >> A[i].gioitinh;
 		scin >> A[i].phat;
-		stringstream(NTNS.substr(0,2))>> A[i].ngaysinh.ngay;
-		stringstream(NTNS.substr(3,2))>> A[i].ngaysinh.thang;
-		stringstream(NTNS.substr(6,4))>> A[i].ngaysinh.nam;
+		stringstream(NTNS.substr(0, 2)) >> A[i].ngaysinh.ngay;
+		stringstream(NTNS.substr(3, 2)) >> A[i].ngaysinh.thang;
+		stringstream(NTNS.substr(6, 4)) >> A[i].ngaysinh.nam;
 	}
 	f.close();
 };
-void OverWriteUser(S_user A[], int N){
+void OverWriteUser(S_user A[], int N) {
 	ofstream f;
 	f.open("users_infor.txt");
 	for (int i = 0;i<N;i++) {//???????????????
+		name_to_str(A[i].hovaten);
 		f << setw(5) << left << A[i].id
-					<< setw(35) << left << A[i].hovaten;
-				if (A[i].ngaysinh.ngay >= 10) f << A[i].ngaysinh.ngay << "/";
-				else f << "0" << A[i].ngaysinh.ngay << "/";
-				if (A[i].ngaysinh.thang >= 10)f << A[i].ngaysinh.thang << "/";
-				else f << "0" << A[i].ngaysinh.thang << "/";
-				f << setw(10) << left << A[i].ngaysinh.nam
-					<< setw(15) << left << A[i].mssv
-					<< setw(35) << left << A[i].email
-					<< setw(5) << left << A[i].gioitinh
-					<< setw(5) << left << A[i].phat
-					<< endl;
+			<< setw(35) << left << A[i].hovaten;
+		if (A[i].ngaysinh.ngay >= 10) f << A[i].ngaysinh.ngay << "/";
+		else f << "0" << A[i].ngaysinh.ngay << "/";
+		if (A[i].ngaysinh.thang >= 10)f << A[i].ngaysinh.thang << "/";
+		else f << "0" << A[i].ngaysinh.thang << "/";
+		f << setw(10) << left << A[i].ngaysinh.nam
+			<< setw(15) << left << A[i].mssv
+			<< setw(35) << left << A[i].email
+			<< setw(5) << left << A[i].gioitinh
+			<< setw(5) << left << A[i].phat
+			<< endl;
 	};
 	f.close();
 };
