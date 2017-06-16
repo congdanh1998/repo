7610ddf38fb5667b2ce201d92ece5cb9818bc22f
congdanh1998 <anh2003maa@gmail.com>
2017-06-16 21:06:01 +0700

    Update xulifile.h

--- xulifile.h
+++ xulifile.h
@@ -6,12 +6,12 @@
 	using namespace std;
 	int get_F_N(string File);//dem so dong cua 1 file
 	void GetFileAccountsData(S_account A[],int & N);//luu het thong tin trong file tai khoan vao mang A voi do dai file la N
-	void OverWriteAccount(S_account A[],int N);
-	void GetFileBookData(S_book A[],int N);
-	void OverWriteBook(S_book A[], int N);
-	void GetfileOrderData(S_Book_Order A[],int &n,bool cp = true);
-	void OverWriteOrder(S_Book_Order A[], int N);
-	void GetFileUserData(S_user A[],int N);
-	void OverWriteUser(S_user A[], int N);
+	void OverWriteAccount(S_account A[],int N);//viet lai file taikhoan
+	void GetFileBookData(S_book A[],int N);//luu het thong tin sach vao mang A[]
+	void OverWriteBook(S_book A[], int N);//viet lai file sach
+	void GetfileOrderData(S_Book_Order A[],int &n,bool cp = true);//lay thong tin file phieu muon
+	void OverWriteOrder(S_Book_Order A[], int N);//viet lai file phieu muon
+	void GetFileUserData(S_user A[],int N);//lay thong tin file user
+	void OverWriteUser(S_user A[], int N);//viet lai file user
 	int songayhientai(int year, int month, int day);//cac tien boi tren mang chi day
 #endif
